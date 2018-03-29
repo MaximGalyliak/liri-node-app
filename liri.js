d59511629240ac64c4f4cfa0361b643287e09fe2
Adler11th <mgoftest@gmail.com>
2018-03-28 17:05:50 -0700

    v 0.0.1

--- liri.js
+++ liri.js
@@ -16,7 +16,7 @@ function input() {
     return name;
 }
 function getMovieInfo(name) {
-    if(name==undefined){
+    if (name == undefined) {
         name = 'Mr.Nobody';
     }
     const REQUEST = require('request');
@@ -49,14 +49,14 @@ function getMovieInfo(name) {
 function getMyTweets() {
     const TWITTER = require('twitter');
     const CLIENT = new TWITTER(KEYS.twitter);
-    FS.appendFileSync('log.txt',`${timeStamp()} -my-tweets- loading tweets...\n`);
-    CLIENT.get('statuses/user_timeline', { q: 'node.js' }, function (error, tweets, response) {
-        if(error){
+    FS.appendFileSync('log.txt', `${timeStamp()} -my-tweets- loading tweets...\n`);
+    CLIENT.get('statuses/user_timeline', { q: 'node.js' }, (error, tweets, response)=>{
+        if (error) {
             console.log('Something went wrong with Twitter...');
-            error.map((err)=>{
+            error.map((err) => {
                 FS.appendFileSync('log.txt', `ERROR: ${err.message}\n`);
             });
-        }else{
+        } else {
             tweets.map((tw) => {
                 console.log(`${tw.created_at}\n${tw.user.name}: ${tw.text}\n`);
             });
@@ -65,30 +65,52 @@ function getMyTweets() {
     });
 }
 function spotifySong(name) {
+    if(name == undefined){
+        name = 'The+Sign+Ace+of+Base';
+    }
+    const SPOTIFY = require('node-spotify-api');
+    const spotify = new SPOTIFY(KEYS.spotify);
+    FS.appendFileSync('log.txt', `${timeStamp()} -spotify-this-song- ${name}\n`);
+    spotify.search({ type: 'track', query: name, limit:'1'}, function (err, data) {
+        if (err) {
+            console.log('Something went wrong with Spotify...');
+            FS.appendFileSync('log.txt',`ERROR: ${err}\n`);
+        } else {
+            let Track = data.tracks.items[0];
+            console.log('ARTIST/s: ');
+            Track.artists.map((artist)=>{
+                console.log(`-${artist.name}`);
+            });
+            console.log(`TRACK: ${Track.name}`);
+            console.log(`ALBUM: ${Track.album.name}`);
+            console.log(`LINK:  ${Track.external_urls.spotify}`);
+            FS.appendFileSync('log.txt', 'Success!\n');
+        }
+    });
     //TODO get spotify keys, set up api calls, log needed info into console and log.txt
 }
-function getWhatItSays(){
-    FS.appendFileSync('log.txt',`${timeStamp()} -do-what-it-says- reading file...\n`)
-    FS.readFile('random.txt','utf8', (err, data)=>{
-        if(err){
+function getWhatItSays() {
+    FS.appendFileSync('log.txt', `${timeStamp()} -do-what-it-says- reading file...\n`)
+    FS.readFile('random.txt', 'utf8', (err, data) => {
+        if (err) {
             console.log('Something went wrong with file...');
-            FS.appendFileSync('log.txt',`ERROR: ${err.message}\n`);
-        }else{
-            FS.appendFileSync('log.txt',`Success!\n`);
+            FS.appendFileSync('log.txt', `ERROR: ${err.message}\n`);
+        } else {
+            FS.appendFileSync('log.txt', `Success!\n`);
             var dataArr = data.split(',');
-            if(dataArr[0]==='do-what-it-says'){
+            if (dataArr[0] === 'do-what-it-says') {
                 console.log("I ain't gonna loop!");
-            }else{
+            } else {
                 runCommand(dataArr[0], dataArr[1]);
             }
         }
     });
 }
-function timeStamp(){
+function timeStamp() {
     const time = new Date;
-    return '@'+time.toDateString()+'/'+time.toLocaleTimeString();
+    return '@' + time.toDateString() + '/' + time.toLocaleTimeString();
 }
-function runCommand(command, name){
+function runCommand(command, name) {
     switch (command) {
         case 'movie-this': getMovieInfo(name); break;
         case 'my-tweets': getMyTweets(); break;
